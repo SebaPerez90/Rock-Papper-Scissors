82c88ed81630d94ef069839b02e346c6e3d1dec8
Seba <sebastianalbertoperez90@gmail.com>
2023-06-01 00:21:03 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://SebaPerez90.github.io/Rock-Papper-Scissors/'
+  base: 'https://SebaPerez90.github.io/Rock-Papper-Scissors'
 })
